a69d6a9c1687809604a79e173aa54c24b4282b1f
shushrutha-sketch <shushrutha080@gmail.com>
2026-08-18 13:02:47 +0530

    Function function | export default

--- Day/Day12/my-react-app/src/App.jsx
+++ Day/Day12/my-react-app/src/App.jsx
@@ -1,9 +1,11 @@
 
 import { Welcome } from "./components/P1.jsx";
+import Greet from "./components/P2.jsx";
 function App() {
   return (
     <div>
         <Welcome />
+         <Greet />
     </div>
   );
 }
