a20c2f957f08a9657f168fb0f33f0d2f1effc15d
shushrutha-sketch <shushrutha080@gmail.com>
2026-08-19 10:54:54 +0530

    Passing Multiple props

--- Day/Day13/my-react-app/src/App.jsx
+++ Day/Day13/my-react-app/src/App.jsx
@@ -1,13 +1,18 @@
 import { Welcome } from "./components/P1";
+import { ProductCard } from "./components/P2";
+import './App.css'
 function App() {
-
-   return (
+  
+  return (
     <>
-     <Welcome name="Rakesh"/>
+     <Welcome name="Shushrutha"/>
+     <ProductCard 
+     name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive Bass"
+     price={15999}
+     image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg" />
+     
     </>
   )
 }
 
 export default App
-
-      
