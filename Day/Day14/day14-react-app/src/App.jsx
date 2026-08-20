9095bbeebb991ba89ab3b2f1f4683d9e66dbd2f2
shushrutha-sketch <shushrutha080@gmail.com>
2026-08-20 13:08:13 +0530

    Hooke Ex 1

--- Day/Day14/day14-react-app/src/App.jsx
+++ Day/Day14/day14-react-app/src/App.jsx
@@ -2,19 +2,22 @@ import './App.css'
 import ClickDemo from "./components/EventHandlerOne";
 import FormDemo from "./components/EventHandlerTwo";
 import ActionButtonDemo from "./components/EventHandlerThree";
+
 import SimpleList from "./components/ListAndKeysOne";
+
+import UserListHook from "./components/HookOne";
 function App() {
   return (
     <>
-      <ClickDemo/>
-
-
+      {/* <ClickDemo/>
+      
       <FormDemo />
-
 
       <ActionButtonDemo />
 
-      <SimpleList />
+      <SimpleList/> */}
+
+      <UserListHook/>
     </>
   )
 }
